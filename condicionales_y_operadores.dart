323ee05e3e3b7243c9e8f3ccbd324e025e98f81c
ANDRES MAURICIO TUBERQUIA CAMPO <andrestuberquia213089@correo.itm.edu.co>
2024-04-17 20:38:06 -0500

    ejercios condiciones

---
+++ condicionales_y_operadores.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  print("Digite el primer numero");
+  double? a = double.tryParse(stdin.readLineSync()!);
+
+  print("Digite el segundo numero");
+  double? b = double.tryParse(stdin.readLineSync()!);
+
+  const indeterminado = "INDETERMINADO";
+
+  if (a != null && b != null) {
+    print(
+        "SUMA : ${a + b}, RESTA : ${a - b}, MULTIPLICACION : ${a * b}, DIVISION : ${(b != 0 ? a / b : indeterminado)}");
+  } else {
+    print("registros numeros ingresados NO válidos");
+  }
+}// fin principal
